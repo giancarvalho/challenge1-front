4d0ae5e6af744b0796d22d68e962ce613b58c113
giancarvalho <giandcarvalho@gmail.com>
2022-02-14 15:38:46 -0300

    componentize ProductsGrid

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,80 @@
+/* eslint-disable react/prop-types */
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+export default function ProductGrid({ productList }) {
+  const [showProducts, setShowProducts] = useState(false);
+
+  useEffect(() => {
+    if (productList.length > 0) {
+      setTimeout(() => setShowProducts(true), 350);
+    } else {
+      setShowProducts(false);
+    }
+  }, [productList]);
+
+  return (
+    <ProductsContainer showProducts={showProducts}>
+      {productList.map((item) => (
+        <ProductCard key={item.id}>
+          <span>M</span>
+          <span>{item.name}</span>
+        </ProductCard>
+      ))}
+    </ProductsContainer>
+  );
+}
+
+const ProductsContainer = styled.ul`
+  display: ${({ showProducts }) => (showProducts ? 'flex' : 'none')};
+  flex-direction: column;
+  width: 80%;
+  max-width: 800px;
+  margin-top: 150px;
+  animation: fadein 300ms linear;
+`;
+
+const ProductCard = styled.li`
+  display: flex;
+  align-items: center;
+  gap: 15px;
+  width: 100%;
+  min-height: 150px;
+  border: 1px solid #808080;
+  background-color: #f3f3f3;
+  border-radius: 8px;
+  margin-bottom: 15px;
+  padding: 0 10px;
+
+  span:first-child {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 89px;
+    height: 75%;
+    min-width: 150px;
+    font-family: "Rowdies", cursive;
+    color: #fff;
+    background-color: #000;
+    border-radius: 8px;
+  }
+
+  span:last-child {
+    display: inline-block;
+    width: 500px;
+    font-size: 28px;
+    word-break: break-word;
+    text-transform: capitalize;
+  }
+
+  @media (max-width: 700px) {
+    span:first-child {
+      min-width: 75px;
+      font-size: 45px;
+    }
+
+    span:last-child {
+    font-size: 14px;
+  }
+  }
+`;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,20 +3,17 @@ import { DebounceInput } from 'react-debounce-input';
 import styled from 'styled-components';
 import { Oval } from 'react-loader-spinner';
 import Header from '../components/Header';
-
 import { getProducts } from '../services/service';
+import ProductGrid from '../components/ProductList';
 
-// TODO usar user ref para fazer pesquisas dinamicas com base em input
 export default function Home() {
   const [productList, setProductList] = useState([]);
   const [searchValue, setSearchValue] = useState('');
   const [isSearching, setIsSearching] = useState(false);
-  const [showProducts, setShowProducts] = useState(false);
   const [warnNotFound, setWarnNotFound] = useState(false);
 
   function cleanSearch() {
     setProductList([]);
-    setShowProducts(false);
     setIsSearching(false);
   }
 
@@ -31,7 +28,6 @@ export default function Home() {
     getProducts(term)
       .then((response) => {
         setProductList(response.data.products);
-        setTimeout(() => setShowProducts(true), 350);
         setIsSearching(false);
       })
       .catch((error) => {
@@ -63,30 +59,23 @@ export default function Home() {
         <InputContainer isSearching={productList.length > 0} warnNotFound={warnNotFound}>
           <Input
             type="search"
-            placeholder="Procure por produtos"
+            placeholder="Pesquise produtos"
             minLength={3}
             debounceTimeout={500}
             value={searchValue}
             onChange={(e) => handleChange(e)}
           />
-          {isSearching && <Oval height={30} width={30} />}
+          {isSearching && <Oval height={30} width={30} color="#000" />}
         </InputContainer>
-        <ProductsContainer showProducts={showProducts}>
-          {productList.map((item) => (
-            <ProductCard key={item.id}>
-              <span>M</span>
-              <span>{item.name}</span>
-            </ProductCard>
-          ))}
-        </ProductsContainer>
+        <ProductGrid productList={productList} />
       </ContentContainer>
     </PageContainer>
   );
 }
 
 const PageContainer = styled.div`
-  margin-top: 100px;
-  height: 100%;
+  padding-top: 100px;
+  height: calc(100% - 100px);
 
   @keyframes fadein {
     0% {
@@ -144,59 +133,5 @@ const InputContainer = styled.div`
     bottom: -40px;
     color: red;
     transition: all 300ms ease-in;
-  }
-`;
-
-const ProductsContainer = styled.ul`
-  display: ${({ showProducts }) => (showProducts ? 'flex' : 'none')};
-  flex-direction: column;
-  width: 80%;
-  max-width: 800px;
-  margin-top: 150px;
-  animation: fadein 300ms linear;
-`;
-
-const ProductCard = styled.li`
-  display: flex;
-  align-items: center;
-  gap: 15px;
-  width: 100%;
-  min-height: 150px;
-  border: 1px solid #808080;
-  background-color: #f3f3f3;
-  border-radius: 8px;
-  margin-bottom: 15px;
-  padding: 0 10px;
-
-  span:first-child {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 89px;
-    height: 75%;
-    min-width: 150px;
-    font-family: "Rowdies", cursive;
-    color: #fff;
-    background-color: #000;
-    border-radius: 8px;
-  }
-
-  span:last-child {
-    display: inline-block;
-    width: 500px;
-    font-size: 28px;
-    word-break: break-word;
-    text-transform: capitalize;
-  }
-
-  @media (max-width: 700px) {
-    span:first-child {
-      min-width: 75px;
-      font-size: 45px;
-    }
-
-    span:last-child {
-    font-size: 14px;
-  }
   }
 `;
